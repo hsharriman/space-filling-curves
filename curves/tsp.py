dd7911a89deee7a3e53208d181e800a3a3882438
hkRho <hrho@olin.edu>
2021-12-10 18:45:23 -0500

    Return list of coordinates

--- curves/tsp.py
+++ curves/tsp.py
@@ -113,7 +113,14 @@ def print_points(points):
         print(point.x, point.y)
         print()
 
+def return_coords(points):
+    coords = []
+    for p in points:
+        coords.append((p.x, p.y))
+    return coords
+
+
 
 P, D = setup(5, 30) # generate 5 points in a 30x30 grid
 total_dist, route = tsp(P, D)
-print_points(route)
+print(return_coords(route))
