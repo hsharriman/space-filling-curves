f6e80bc2dd43ebb95edecfde4978f88f4f1989c8
Hwei-Shin Harriman <hwei.shin.harriman@gmail.com>
2021-12-12 16:43:24 -0500

    commented hilbert.py

--- curves/hilbert.py
+++ curves/hilbert.py
@@ -3,11 +3,26 @@ import turtle
 import time
 
 def fwd(t, dist, points):
+    """
+    Move turtle forward and add new position to list
+
+    :param t: turtle.Turtle instance
+    :param dist: int, distance for turtle to move forward
+    :param points: [tuple], current list of (x,y) coordinates
+    :returns: None
+    """
     t.forward(dist)
     points.append(tuple(t.pos()))
 
 
 def hilbert(n, size):
+    """
+    Create a Hilbert curve using turtle graphics
+
+    :param n: int, recursion depth for Hilbert curve
+    :param size: int, scalar for unit square that Hilbert curve is contained in
+    :returns: list of (x,y) coords corresponding to all points the turtle has visited
+    """
     t = turtle.Turtle()
     t.penup()
     t.goto(-size / 2.0, size / 2.0)
@@ -16,23 +31,38 @@ def hilbert(n, size):
     return points
 
 def recurse(t, n, dirc, dist, points):
+    """
+    Recursive function for Hilbert Curve
+
+    :param t: turtle.Turtle instance
+    :param n: int, current recursion depth
+    :param dirc: int, direction to turn (1 or -1)
+    :param dist: int, distance for turtle to travel forwards
+    :param points: [tuple], array of (x,y) coordinates of turtle's position
+    :returns: [tuple], current list of points
+    """
     if n == 0:
         return points
-    t.left(90 * dirc)
+
+    #turn turtle 90 deg left. if deg =-1 then it will turn right instead
+    t.left(90 * dirc)  
     recurse(t, n-1, -dirc, dist, points)
+
+    #move turtle forward after completing recurse() call
     fwd(t, dist, points)
     t.right(90 * dirc)
     recurse(t, n-1, dirc, dist, points)
+
     fwd(t, dist, points)
     recurse(t, n-1, dirc, dist, points)
+
     t.right(90 * dirc)
     fwd(t, dist, points)
     recurse(t, n-1, -dirc, dist, points)
+
+    #need to turn turtle left after completing iteration
     t.left(90 * dirc)
     return points
-
-def plot():
-    s = turtle.getscreen()
 
 
 if __name__ == "__main__":
